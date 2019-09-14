9494f452417be35af7b8918ff8397be26febf6e5
kolaveridi <apple@APPLEs-MacBook-Air-2.local>
2019-09-14 19:09:10 +0530

    videoplayer state changing fix

--- src/containers/Movies/MoviesList/Lists.js
+++ src/containers/Movies/MoviesList/Lists.js
@@ -1,57 +1,81 @@
- import React from 'react';
- import './List.css';
- import YouTube from "react-youtube";
+import React from 'react';
+import './List.css';
+import YouTube from "react-youtube";
 
- class MovieList extends React.Component{
+class MovieList extends React.Component {
 
-   onClick =() =>{
-     alert('hello');
-     console.log("working");
-   }
-   hiddenClick=(youtubeVideoUrl)=>{
-    
-     console.log('clicked',youtubeVideoUrl )
-   }
+  state = {
+    videoClicked: false,
+    videoUrl: null
+  }
+  onClick = () => {
+    alert('hello');
+    console.log("working");
+  }
+  hiddenClick = (youtubeVideoUrl) => {
+    this.setState({
+      videoClicked: true,
+      videoUrl: youtubeVideoUrl
 
-   _onReady(event) {
+    })
+
+    console.log('clicked', youtubeVideoUrl)
+  }
+
+  _onReady(event) {
     // access to player in all event handlers via event.target
     event.target.pauseVideo();
   }
 
-
-  render(){
-    let data=[];
-    const{movies} = this.props;
-    const opts = {
-      height: "300",
-      width: "150",
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
-      }
-    };
+  changeState = () => {
+    this.setState({videoClicked:true,videoUrl:null});
+  }
 
 
-    return(
+  render() {
+    const { videoClicked, videoUrl } = this.state;
+    let autoPlayVideoUrl= videoUrl+"?autoplay=1";
+    console.log("autoPlayVideoUrl",autoPlayVideoUrl);
+    let data = [];
+    const { movies } = this.props;
+
+
+    return (
       <div className="moviesRoot">
+
+         {
+                   videoClicked ?
+                    <div className="play">
+                       <iframe play width="200px" height="200px" src={autoPlayVideoUrl} >
+                   </iframe>
+                   </div>
+                   :null
+
+            } 
+         
         {
-          movies && movies.map((movie,index)=>{
+          movies && movies.map((movie, index) => {
             let youTubeUrl = movie.TrailerURL;
 
-             let  i, r, rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
-             r = youTubeUrl.match(rx);
-             let youTubeTrailerId=r[1];
-             let youtubeVideoUrl=`https://www.youtube.com/embed/${youTubeTrailerId}`;
+            let i, r, rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+            r = youTubeUrl.match(rx);
+            let youTubeTrailerId = r[1];
+            let youtubeVideoUrl = `https://www.youtube.com/embed/${youTubeTrailerId}`;
 
             return (
-              <div   key={movie.EventCode}>
+              <div key={movie.EventCode}>
 
-              <div  className="movieItem">
-                <iframe width="150" height="220" src={youtubeVideoUrl} >
-               </iframe> 
-                <div onClick={()=>this.hiddenClick(youtubeVideoUrl)} className="overlay">
-                  </div>
-              </div>
+                <div className="movieItem">
+                  {
+                    
+                      <div>
+                        <iframe width="150" height="220" src={youtubeVideoUrl}>
+                        </iframe>
+                        <div onClick={() => this.hiddenClick(youtubeVideoUrl)} className="overlay">
+                        </div>
+                      </div>
+                  }
+                </div>
               </div>
             )
           })
@@ -61,5 +85,5 @@
     )
   }
 
- }
- export default MovieList;
+}
+export default MovieList;
